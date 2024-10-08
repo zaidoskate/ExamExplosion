1e5c206e24d0efcb6d05c4eb8dac9bd2c48df7bd
Zaid <zaidoskate@hotmail.com>
2024-10-07 21:01:41 -0600

    primer commit

---
+++ ExamExplosion/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExamExplosion
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        App()
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("es-MX");
+            //System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+        }
+    }
+}
---
+++ ExamExplosion/GamePreferences.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExamExplosion
+{
+    /// <summary>
+    /// Lógica de interacción para GamePreferences.xaml
+    /// </summary>
+    public partial class GamePreferences : Page
+    {
+        public GamePreferences()
+        {
+            InitializeComponent();
+            InitializeSliderValues();
+        }
+
+        private void InitializeSliderValues()
+        {
+            MaxPlayersValue.Text = ((int)MaxPlayersSlider.Minimum).ToString();
+            TimePerTurnValue.Text = ((int)TimePerTurnSlider.Minimum).ToString();
+            MaxHPValue.Text = ((int)MaxHPSlider.Minimum).ToString();
+        }
+        private void MaxPlayersSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (MaxPlayersValue != null)
+            {
+                MaxPlayersValue.Text = ((int)e.NewValue).ToString();
+            }
+        }
+
+        private void TimePerTurnSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (TimePerTurnValue != null)
+            {
+                TimePerTurnValue.Text = ((int)e.NewValue).ToString();
+            }
+        }
+
+        private void MaxHPSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (MaxHPValue != null)
+            {
+                MaxHPValue.Text = ((int)e.NewValue).ToString();
+            }
+        }
+    }
+}
